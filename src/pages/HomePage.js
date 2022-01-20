f24b2196be2b1b7c637f81326a53b69101115226
Yelena Guthrie <yelena.guthrie@gmail.com>
2022-01-19 21:44:32 -0600

    Initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in diam
+      erat. Nulla bibendum lacus id purus lacinia, ut commodo justo accumsan.
+      Nam eu nulla sed mauris auctor lacinia eget in dui. Proin ac pharetra
+      lectus, ut dignissim odio. Quisque elementum orci non orci ultricies, ut
+      sollicitudin nulla tempus. Cras tellus enim, pretium non enim molestie,
+      ultricies viverra mi. Nunc accumsan leo nec neque accumsan, vel commodo
+      erat scelerisque. Nunc varius diam eu cursus dignissim. Morbi purus
+      tortor, pellentesque non suscipit ut, porta ut lorem.
+    </p>
+    <p>
+      Praesent vel venenatis tellus, at congue neque. Quisque semper tempor
+      nulla non auctor. Phasellus nibh metus, euismod sit amet orci ac, feugiat
+      interdum mi. Pellentesque non eros sed nisl vestibulum pellentesque eu
+      eget ex. Curabitur suscipit arcu at tellus lobortis, vitae gravida tellus
+      malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Maecenas nec lectus magna.
+    </p>
+    <p>
+      Vivamus interdum elit quis dui lobortis, id elementum orci pulvinar. Fusce
+      sed mauris neque. Morbi blandit sed risus id sollicitudin. Morbi imperdiet
+      mi non tempus aliquet. Vestibulum ante ipsum primis in faucibus orci
+      luctus et ultrices posuere cubilia curae; Cras sit amet velit at justo
+      euismod convallis sed a erat. Vivamus maximus velit a pharetra accumsan.
+      Mauris aliquam rutrum libero, sed sodales augue pellentesque accumsan. Ut
+      erat est, posuere aliquet tortor sed, egestas condimentum dolor.
+    </p>
+  </>
+);
+
+export default HomePage;
